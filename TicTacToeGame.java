84d018281dcd73884b9f233f9381f821c39a5672
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-28 08:55:52 +0530

    Added- UC1 to create a TicTacToe board game

---
+++ TicTacToeGame.java
@@ -0,0 +1,22 @@
+package com.Bridgelabz.TicTacToeGame;
+import java.util.*;
+
+
+public class TicTacToeGame{
+
+	public static void main(String[] args) {
+
+		System.out.print("Welcome to TicTacToe game !!! ");
+
+			game();
+	}
+
+	public static void game() {
+
+		char[] tic = new char[10];
+		for(int i=1;i<10;i++) {
+			tic[i]=' ';
+		}
+		
+	}
+}
