8e33786ca493178e06a92036b57a1bb344207345
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-28 09:14:47 +0530

    Added-new branch UC1 to master of creating board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -6,14 +6,12 @@ public class TicTacToeGame{
 
 	public static void main(String[] args) {
 
-		System.out.print("Welcome to TicTacToe game !!! ");
-		System.out.print("TicTacToegame begin !!! ");
-
-
-			game();
+		System.out.println("Welcome to TicTacToe game!!! ");
+		System.out.println(" ");
+		
+			createBoard();
 	}
-
-	public static void game() {
+	public static void createBoard() {
 
 		char[] tic = new char[10];
 		for(int i=1;i<10;i++) {
